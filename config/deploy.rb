8d3672307a4fe9262b28049039cda2106afead9f
Bob Burbach <bburbac@wested.org>
2009-06-12 10:31:05 -0700

    Bringing ruby enterprise up to date (Bignum DOS release) and add calipers silver to the ip list

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,9 @@
 # Fill slice_url in - where you're installing your stack to
 #role :app, "100.10.100.100"
-#role :app, "173.45.225.212"  # Calipers Production
-role :app, "173.45.238.128"   # Calipers Staging
+#role :app, "173.45.225.212"  # Calipers Production(Gold)
+role :app, "173.45.228.2"    # Calipers QC (Silver)
+#role :app, "173.45.238.128"   # Calipers Staging (Bronze)
+
 #role :app, "173.45.236.122"   # CSCS Production
 
 # Fill user in - if remote user is different to your local user
